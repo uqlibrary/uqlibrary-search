a4134e00fc62efc1f9cdde1c95e0fc21faec6ddb
jannakha <j_malikova@yahoo.com>
2016-02-19 16:43:32 +1000

    added suggestions

--- elements/uqlibrary-search.js
+++ elements/uqlibrary-search.js
@@ -5,10 +5,31 @@
       api : {
           type: Object,
           value: {
-              summonApi: 'https://d3nm82zk9ronst.cloudfront.net/metadata/suggest/suggest',
-              lrApi: 'https://app.library.uq.edu.au/api/search_suggestions?type=learning_resource',
-              examsApi: 'https://app.library.uq.edu.au/api/search_suggestions?type=exam_paper',
-              databaseApi: 'https://app.library.uq.edu.au/api/search_suggestions?type=database'
+              summonApi: {
+                url: 'https://d3nm82zk9ronst.cloudfront.net/metadata/suggest/suggest',
+                params:
+                  { prefix: '' }
+              },
+              lrApi: {
+                url: 'https://app.library.uq.edu.au/api/search_suggestions',
+                params: {
+                  type : 'learning_resource',
+                  prefix: '' }
+              },
+              examsApi: {
+                url: 'https://app.library.uq.edu.au/api/search_suggestions',
+                params: {
+                  type : 'exam_paper',
+                  prefix: ''
+                }
+              },
+              databaseApi: {
+                url: 'https://app.library.uq.edu.au/api/search_suggestions',
+                params: {
+                  type: 'database',
+                  prefix: ''
+                }
+              }
           }
       },
       links : {
@@ -23,10 +44,13 @@
           }
       },
       sources: {
-          type: Array
+        type: Array
       },
       suggestions: {
-          type: Array
+        type: Array,
+        value: function() {
+          return [];
+        }
       },
       selectedSourceIndex: {
         type: Number,
@@ -36,6 +60,19 @@
       selectedSource: {
         type: Object,
         notify: true
+      },
+      selectedSourceApi: { type: String },
+      keyword: {
+        type: String,
+        observer: '_keywordChanged'
+      },
+      maxRecentSuggestions: {
+        type: Number,
+        value: 2
+      },
+      maxRecentSearches: {
+        type: Number,
+        value: 10
       }
     },
 
@@ -51,109 +88,206 @@
       console.log('looking ...');
     },
 
-    ready: function() {
-        var self = this;
-        var defaultHelpLinks = [
-            {
-                title: 'Search help',
-                url: 'https://www.library.uq.edu.au/search'
-            },
-            {
-                title: 'Advanced search',
-                url: 'http://uq.summon.serialssolutions.com/#!/advanced'
-            }
-        ];
+    _sourceSelected: function(e) {
+      if (this.selectedSource && !this.selectedSource.autoSuggest)
+        this.suggestions = [];
 
-        this.sources = [
-            { name: 'Library',
-                type: 'all',
-                url: this.links.summon,
-                urlAppend: '&fvf=ContentType,Newspaper%20Article,t%7CContentType,Book%20Review,t',
-                autoSuggest: true,
-                api: this.api.summonApi,
-                icon: 'social:public',
-                inputPlaceholder: 'Find books, articles, databases, conferences and more',
-                helpLinks: defaultHelpLinks
-            },
-            { name: 'Books',
-                type: 'books',
-                url: this.links.summon,
-                urlAppend: '&fvf=ContentType,Book%20%2F%20eBook,f%7CContentType,Newspaper%20Article,t%7CContentType,Book%20Review,t%7CContentType,Book%20Chapter,f',
-                autoSuggest: true,
-                api: this.api.summonApi,
-                icon: 'communication:import-contacts',
-                inputPlaceholder: 'Enter a keyword, title, author, etc',
-                helpLinks: defaultHelpLinks
-            },
-            { name: 'Journal articles',
-                type: 'journal_articles',
-                url: this.links.summon,
-                urlAppend: '&fvf=ContentType,Newspaper%20Article,t%7CContentType,Book%20Review,t%7CContentType,Journal%20Article,f%7CContentType,Magazine%20Article,f',
-                autoSuggest: true,
-                api: this.api.summonApi,
-                icon: 'social:school',
-                inputPlaceholder: 'Enter a keyword, article title, author, publication, etc',
-                helpLinks: defaultHelpLinks
-            },
-            { name: 'Multimedia',
-                type: 'multimedia',
-                url: this.links.summon,
-                urlAppend: '&fvf=ContentType,Newspaper%20Article,t%7CContentType,Book%20Review,t%7CContentType,Audio%20Recording,f%7CContentType,Spoken%20Word%20Recording,f%7CContentType,Video%20Recording,f%7CContentType,Streaming%20Video,f%7CContentType,Slide,f%7CContentType,Photograph,f%7CContentType,Music%20Recording,f%7CContentType,Music%20Score,f%7CContentType,Painting,f%7CContentType,Sheet%20Music,f%7CContentType,Poster,f%7CContentType,Art,f%7CContentType,Compact%20Disc,f%7CContentType,Drawing,f%7CContentType,Graphic%20Arts,f%7CContentType,Image,f%7CContentType,Kit,f',
-                autoSuggest: true,
-                api: this.api.summonApi,
-                icon: 'icons:theaters',
-                inputPlaceholder: 'Enter a keyword, title, cast, crew, composer, artist, etc',
-                helpLinks: defaultHelpLinks
-            },
-            { name: 'Journals',
-                type: 'journals',
-                url: this.journals,
-                autoSuggest: true,
-                api: this.api.summonApi,
-                icon: 'editor:insert-drive-file',
-                inputPlaceholder: 'Enter journal or newspaper title',
-                helpLinks: [ { title: 'Browse journals', url: 'https://app.library.uq.edu.au/#/journals' } ]
-            },
-            { name: 'Databases',
-                type: 'databases',
-                url: this.links.database,
-                autoSuggest: true,
-                api: this.api.database,
-                icon: 'editor:format-align-justify',
-                inputPlaceholder: 'Enter a keyword or database name',
-                helpLinks: [
-                    { title: 'Database help', url: 'https://www.library.uq.edu.au/v/6559' },
-                    { title: 'Browse databases', url: 'https://app.library.uq.edu.au/#/gateways/database' }
-                ]
-            },
-            { name: 'Catalogue',
-                type: 'catalogue',
-                url: this.links.catalogue,
-                autoSuggest: false,
-                icon: 'icons:inbox',
-                inputPlaceholder: 'Enter a keyword, title, author, etc',
-                helpLinks: [{title: 'Advanced catalogue search', url: 'https://library.uq.edu.au/search~S7/X'}]
-            },
-            { name: 'Past exam papers',
-                type: 'exam_papers',
-                url: this.links.exams,
-                autoSuggest: true,
-                api: this.api.examsApi,
-                icon: 'icons:assessment',
-                inputPlaceholder: 'Enter a course code',
-                helpLinks: [{title: 'Browse courses', url: 'https://www.library.uq.edu.au/exams/search.html'}]
-            },
-            { name: 'Course reading lists',
-                type: 'learning_resources',
-                url: this.lr, autoSuggest: true,
-                api: this.api.lrApi,
-                icon: 'icons:list',
-                inputPlaceholder: 'Enter a course code',
-                helpLinks: [{title: 'Browse courses', url: 'http://lr.library.uq.edu.au/index.html?browse'}]
-            }
-        ];
-        this.selectedSource = this.sources[this.selectedSourceIndex];
+      this.$.inputKeyword.focus();
     },
+
+    _keywordChanged: function(newValue, oldValue) {
+      if (this.keyword.length > 2 && this.selectedSource.autoSuggest) {
+
+        this.$.jsonpQuery.url = this.selectedSource.api.url;
+        this.selectedSource.api.params.prefix = encodeURIComponent(this.keyword);
+        this.$.jsonpQuery.params = this.selectedSource.api.params;
+
+        if(!this.$.jsonpQuery.loading)
+          this.$.jsonpQuery.generateRequest();
+      }
+    },
+
+    _suggestionsLoaded : function(e) {
+      this.suggestions = this._processSuggestions(e.detail.result ? e.detail.result : e.detail);
+      console.log(this.suggestions.length);
+      this.$.menuSuggestions.open();
+    },
+
+    _suggestionsLoadingError : function(e) {
+      console.log(e);
+    },
+
+    _processSuggestions: function (suggestions) {
+      var processed = [];
+      var api = this.selectedSource.api;
+      var type = this.selectedSource.type;
+
+      if (api.url === this.api.summonApi.url) {
+        suggestions.forEach(function (s) {
+          s.origName = s.name;
+          s.type = type;
+          processed.push(s);
+        });
+      }
+      else if (api.params.type === this.api.lrApi.params.type) {
+        suggestions.forEach(function (s) {
+          s.origName = s.name;
+          s.type = type;
+          s.name = s.name + ' (' + (s.course_title ? (s.course_title + '|') : '')  +
+              (s.campus ? s.campus + ', ' : '') +
+              (s.period ? s.period.toLowerCase() : '') +')';
+          processed.push(s);
+        });
+      }
+      else if (api.params.type === this.api.examsApi.params.type) {
+        suggestions.forEach(function (s) {
+          s.origName = s.name;
+          s.type = type;
+          s.name = s.name +' ('+ (s.course_title ? (s.course_title) : '') + ')';
+          processed.push(s);
+        });
+      }
+      else if (api.params.type === this.api.databaseApi.params.type) {
+        suggestions.forEach(function (s) {
+          s.origName = s.name;
+          s.type = type;
+          s.name = s.name + ' (' + (s.type == 'database' ? 'Database' : 'Subject Area')+')';
+          processed.push(s);
+        });
+      }
+
+      var result = this._recentSuggestions();
+      processed.forEach(function (p) {
+        var exists = result.filter(function (v) {
+          return (v.name === p.name);
+        });
+        if (! exists.length) {
+          result.push(p);
+        }
+      });
+
+      return result;
+    },
+
+    _recentSuggestions: function () {
+      var recent = [];
+      var that = this;
+      if (this.recent && this.recent.searches && this.recent.searches.hasOwnProperty(this.selectedFilter)) {
+        recent = this.recent.searches[this.selectedFilter].filter(function (v) {
+          return (v.name.toLowerCase().indexOf(that.searchText.toLowerCase()) !== -1);
+        });
+      }
+      return recent.sort(function (a, b) {
+        return a.name.length - b.name.length;
+      }).slice(0, this.maxRecentSuggestions);
+    },
+
+    ready: function() {
+      var self = this;
+      var defaultHelpLinks = [
+        {
+          title: 'Search help',
+          url: 'https://www.library.uq.edu.au/search'
+        },
+        {
+          title: 'Advanced search',
+          url: 'http://uq.summon.serialssolutions.com/#!/advanced'
+        }
+      ];
+
+      this.sources = [
+        { name: 'Library',
+          type: 'all',
+          url: this.links.summon,
+          urlAppend: '&fvf=ContentType,Newspaper%20Article,t%7CContentType,Book%20Review,t',
+          autoSuggest: true,
+          api: this.api.summonApi,
+          icon: 'social:public',
+          inputPlaceholder: 'Find books, articles, databases, conferences and more',
+          helpLinks: defaultHelpLinks
+        },
+        { name: 'Books',
+          type: 'books',
+          url: this.links.summon,
+          urlAppend: '&fvf=ContentType,Book%20%2F%20eBook,f%7CContentType,Newspaper%20Article,t%7CContentType,Book%20Review,t%7CContentType,Book%20Chapter,f',
+          autoSuggest: true,
+          api: this.api.summonApi,
+          icon: 'communication:import-contacts',
+          inputPlaceholder: 'Enter a keyword, title, author, etc',
+          helpLinks: defaultHelpLinks
+        },
+        { name: 'Journal articles',
+          type: 'journal_articles',
+          url: this.links.summon,
+          urlAppend: '&fvf=ContentType,Newspaper%20Article,t%7CContentType,Book%20Review,t%7CContentType,Journal%20Article,f%7CContentType,Magazine%20Article,f',
+          autoSuggest: true,
+          api: this.api.summonApi,
+          icon: 'social:school',
+          inputPlaceholder: 'Enter a keyword, article title, author, publication, etc',
+          helpLinks: defaultHelpLinks
+        },
+        { name: 'Multimedia',
+          type: 'multimedia',
+          url: this.links.summon,
+          urlAppend: '&fvf=ContentType,Newspaper%20Article,t%7CContentType,Book%20Review,t%7CContentType,Audio%20Recording,f%7CContentType,Spoken%20Word%20Recording,f%7CContentType,Video%20Recording,f%7CContentType,Streaming%20Video,f%7CContentType,Slide,f%7CContentType,Photograph,f%7CContentType,Music%20Recording,f%7CContentType,Music%20Score,f%7CContentType,Painting,f%7CContentType,Sheet%20Music,f%7CContentType,Poster,f%7CContentType,Art,f%7CContentType,Compact%20Disc,f%7CContentType,Drawing,f%7CContentType,Graphic%20Arts,f%7CContentType,Image,f%7CContentType,Kit,f',
+          autoSuggest: true,
+          api: this.api.summonApi,
+          icon: 'icons:theaters',
+          inputPlaceholder: 'Enter a keyword, title, cast, crew, composer, artist, etc',
+          helpLinks: defaultHelpLinks
+        },
+        { name: 'Journals',
+          type: 'journals',
+          url: this.journals,
+          autoSuggest: true,
+          api: this.api.summonApi,
+          icon: 'editor:insert-drive-file',
+          inputPlaceholder: 'Enter journal or newspaper title',
+          helpLinks: [ { title: 'Browse journals', url: 'https://app.library.uq.edu.au/#/journals' } ]
+        },
+        { name: 'Databases',
+          type: 'databases',
+          url: this.links.database,
+          autoSuggest: true,
+          api: this.api.databaseApi,
+          icon: 'editor:format-align-justify',
+          inputPlaceholder: 'Enter a keyword or database name',
+          helpLinks: [
+            { title: 'Database help', url: 'https://www.library.uq.edu.au/v/6559' },
+            { title: 'Browse databases', url: 'https://app.library.uq.edu.au/#/gateways/database' }
+          ]
+        },
+        { name: 'Catalogue',
+          type: 'catalogue',
+          url: this.links.catalogue,
+          autoSuggest: false,
+          icon: 'icons:inbox',
+          inputPlaceholder: 'Enter a keyword, title, author, etc',
+          helpLinks: [{title: 'Advanced catalogue search', url: 'https://library.uq.edu.au/search~S7/X'}]
+        },
+        { name: 'Past exam papers',
+          type: 'exam_papers',
+          url: this.links.exams,
+          autoSuggest: true,
+          api: this.api.examsApi,
+          icon: 'icons:assessment',
+          inputPlaceholder: 'Enter a course code',
+          helpLinks: [{title: 'Browse courses', url: 'https://www.library.uq.edu.au/exams/search.html'}]
+        },
+        { name: 'Course reading lists',
+          type: 'learning_resources',
+          url: this.lr, autoSuggest: true,
+          api: this.api.lrApi,
+          icon: 'icons:list',
+          inputPlaceholder: 'Enter a course code',
+          helpLinks: [{title: 'Browse courses', url: 'http://lr.library.uq.edu.au/index.html?browse'}]
+        }
+      ];
+      this.selectedSource = this.sources[this.selectedSourceIndex];
+
+      this.$.menuSuggestions.positionTarget = this.$.inputKeyword;
+
+    }
 
   });
 })();
